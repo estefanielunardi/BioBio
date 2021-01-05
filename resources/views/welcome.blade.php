e039112d8109a2ed1f5af6bef9931fdbd54065a9
Eni <estefanielunardi@users.noreply.github.com>
2021-01-05 15:52:59 +0100

    welcome page style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,45 +8,20 @@
 
         <title>BioBio</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-
-        <style>
-            body {
-                font-family: 'Nunito';
-                /* background-color: black; */
-            }
-        </style>
     </head>
 
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+    <body>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endauth
+        <div id="containerButtons">
+            <div class="container justify-content-center">
+                <div>
+                    <button type="button" class="btn btn-info col align-self-center text-dark" id="butonBiobio">BioBio</button>
                 </div>
-            @endif
-
-    <div class="container justify-content-center">
-            <div>
-                <button type="button" class="btn btn-info col align-self-center">BioBio</button>
+                <div>
+                    <button type="button" class="btn btn-warning col align-self-center text-dark" id="butonPlay" >Play</button>
+                </div>
             </div>
-
-            <div>
-                <button type="button" class="btn btn-warning col align-self-center">PLAY</button>
-            </div>
-    </div>
-
         </div>
+
     </body>
 </html>
